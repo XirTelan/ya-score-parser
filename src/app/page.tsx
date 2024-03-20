5ae45bdbdbffca4525f4011ba1e8a690273f951f
Xir <prssergey@gmail.com>
2024-03-20 18:23:10 +0300

    refactor: remove unused code and fixe time view
    refactor:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { buildRaiting, getStatus } from "@/actions/actions";
 import Leaderboard from "@/components/Leaderboard";
-import UpdateInProgress from "@/components/UpdateInProgress";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
@@ -11,16 +10,15 @@ export default async function Home() {
   Во-вторых, парсятся только первые 30 страниц каждого контеста А
   в-третьих, создатель, возможно -🦀`;
   const contest: any = await getStatus();
-  if (contest && contest.status == "update")
-    return (
-      <div className="flex flex-col min-h-screen w-full justify-center items-center">
-        <UpdateInProgress />
-      </div>
-    );
   const resData = await buildRaiting();
+  const date = new Date(contest.date);
+
   return (
     <main className="flex flex-col items-center">
       <div className="pt-4 container">
+        {contest && (
+          <p>Последнее обновление: {`${date.toLocaleString("ru-RU")}`}</p>
+        )}
         <Leaderboard data={resData} />
         <p className="text-sm text-slate-400 ">{disclaimer}</p>
       </div>
