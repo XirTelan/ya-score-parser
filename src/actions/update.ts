0ff847e459d6aaa23c709f3e7b5e31fa677a8a4f
Xir <prssergey@gmail.com>
2024-03-20 22:10:11 +0300

    refactor:

--- src/actions/update.ts
+++ src/actions/update.ts
@@ -6,7 +6,6 @@ import { Contests } from "../../types";
 
 export async function updateAll(from: number, to: number) {
   const promises = [];
-  const res = await initUpdate();
   if (!res) return;
   await dbConnect();
 
@@ -16,35 +15,13 @@ export async function updateAll(from: number, to: number) {
   promises.push(fetchLeaderbord(from, to, "contest4"));
   const results = await Promise.all(promises);
 
-  await Contest.updateOne({ contest: 1 }, { step: "parsing" });
   let index = 0;
   for (index; index < results.length; index++) {
     await updateContest(results[index], `contest${index}` as Contests);
   }
 
   console.log("All updated");
-  await Contest.updateOne({ contest: 1 }, { date: Date.now(), status: "ok" });
-  console.log("Status - ok");
 }
-
-const initUpdate = async () => {
-  await dbConnect();
-  const contest = await Contest.findOne({ contest: 1 });
-  if (!contest) Contest.create({ contest: 1, date: Date.now() });
-  const differenceInMillis = Math.abs(Date.now() - contest.date);
-  const differenceInHours = differenceInMillis / (1000 * 60 * 60);
-  if (contest.status == "update") {
-    if (differenceInHours > 0.2)
-      await Contest.updateOne({ contest: 1 }, { status: "ok", step: "ok" });
-  }
-  console.log("differenceInHours", differenceInHours);
-
-  await Contest.updateOne(
-    { contest: 1 },
-    { date: Date.now(), status: "ok", step: "ok" }
-  );
-  return true;
-};
 
 export const updateOne = async (
   contest: Contests,
@@ -52,13 +29,7 @@ export const updateOne = async (
   to: number
 ) => {
   await dbConnect();
-  const status = await initUpdate();
-  if (!status) return;
-
   const res = await fetchLeaderbord(from, to, contest);
   await updateContest(res, contest as Contests);
-  await Contest.updateOne(
-    { contest: 1 },
-    { date: Date.now(), status: "ok", step: "ok" }
-  );
+  console.log("All updated");
 };
